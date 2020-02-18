a585472833fca84b1e49b6c38be615a2bce652c6
Muhammad Owais Warsi <owaiskhan148@gmail.com>
2020-02-19 02:17:54 +0500

    minor updates

--- src/Component/Dashboard/index.js
+++ src/Component/Dashboard/index.js
@@ -18,67 +18,6 @@ class Dashboard extends Component {
     });
   };
 
-  submitFunc = () => {
-    let { stage_code_add_mutation } = this.props;
-    let { input } = this.state;
-    this.setState({
-      addLoader: true
-    });
-    let date = new Date().toISOString().split("T");
-    let time = date[1].split(".")[0];
-    let timestamp = `${date[0]} ${time}`;
-
-    stage_code_add_mutation({
-      variables: {
-        code_string: input,
-        timestamp
-      }
-    })
-      .then(res => {
-        let old_data = JSON.parse(localStorage.getItem("data"));
-        if (old_data) {
-          old_data.push({
-            code_string: input,
-            timestamp
-          });
-          localStorage.setItem("data", JSON.stringify(old_data));
-          this.setState({
-            data: old_data,
-            input: "",
-            addLoader: false
-          });
-        } else {
-          let new_data = [
-            {
-              code_string: input,
-              timestamp
-            }
-          ];
-          localStorage.setItem(
-            "data",
-            JSON.stringify([
-              {
-                code_string: input,
-                timestamp
-              }
-            ])
-          );
-          this.setState({
-            data: new_data,
-            addLoader: false,
-            input: ""
-          });
-        }
-      })
-      .catch(err => {
-        console.log(err, "err");
-        this.setState({
-          input: "",
-          addLoader: false
-        });
-      });
-  };
-
   componentDidMount() {
     let data = JSON.parse(localStorage.getItem("data"));
     this.setState({
@@ -86,29 +25,8 @@ class Dashboard extends Component {
     });
 
     this.props.subscribeToNewComments();
-    this.props.subscribeToNewComments1()
+    this.props.subscribeToNewComments1();
   }
-
-  removeFunc = (item, index) => {
-    let { stage_code_delete_mutation } = this.props;
-    stage_code_delete_mutation({
-      variables: {
-        code_string: item.code_string,
-        timestamp: item.timestamp
-      }
-    })
-      .then(res => {
-        let old_data = JSON.parse(localStorage.getItem("data"));
-        if (old_data) {
-          old_data.splice(index, 1);
-          localStorage.setItem("data", JSON.stringify(old_data));
-          this.setState({ data: old_data });
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
 
   render() {
     let { loader, items } = this.props;
@@ -131,7 +49,7 @@ class Dashboard extends Component {
         <div className="cards_main_div">
           {items && items.length
             ? items.map((item, index) => {
-                if (item.code_string !== "null" && item.timestamp !== "null1")
+                if (item.code_string !== "null" && item.timestamp !== "null1") {
                   return (
                     <Col
                       key={index}
@@ -144,6 +62,8 @@ class Dashboard extends Component {
                       <div className="tex-div">{item.code_string}</div>
                     </Col>
                   );
+                }
+                return "";
               })
             : ""}
         </div>
--- src/Container/Dashboard/index.js
+++ src/Container/Dashboard/index.js
@@ -2,11 +2,8 @@ import React from "react";
 import Dashboard from "../../Component/Dashboard";
 import "./index.css";
 import { connect } from "react-redux";
-import { Mutation } from "react-apollo";
 import routeAction from "../../store/actions/routeAction";
 import { logout } from "../../Service/AuthService";
-import { stage_code_add, stage_code_delete } from "../../Config/Queries";
-import { AppSync } from "./../../Config/graphql-config";
 import { Query } from "react-apollo";
 import {
   stage_code_query,
@@ -50,104 +47,56 @@ class DashboardContainer extends React.Component {
     let { loader } = this.state;
     return (
       <div className="MainContainer">
-        <Mutation client={AppSync} mutation={stage_code_add}>
-          {stage_code_add_mutation => {
+        <Query query={stage_code_query}>
+          {({ data, subscribeToMore }) => {
+            let items = data && data.stage_code ? data.stage_code : [];
             return (
-              <Mutation client={AppSync} mutation={stage_code_delete}>
-                {stage_code_delete_mutation => {
-                  return (
-                    <Query query={stage_code_query}>
-                      {({ data, subscribeToMore }) => {
-                        let items =
-                          data && data.stage_code ? data.stage_code : [];
-                        let getData = JSON.parse(localStorage.getItem("data"));
-                        Array.prototype.push.apply(items, getData);
-                        const uniqueObjects = [
-                          ...new Map(
-                            items.map(item => [item.timestamp, item])
-                          ).values()
-                        ];
-                        localStorage.setItem(
-                          "data",
-                          JSON.stringify(uniqueObjects)
-                        );
-                        return (
-                          <div>
-                            <Dashboard
-                              logout={this.logout}
-                              stage_code_add_mutation={stage_code_add_mutation}
-                              items={uniqueObjects}
-                              stage_code_delete_mutation={
-                                stage_code_delete_mutation
+              <div>
+                <Dashboard
+                  logout={this.logout}
+                  items={items}
+                  subscribeToNewComments={() =>
+                    subscribeToMore({
+                      document: on_Add_stage_code,
+                      updateQuery: (prev, { subscriptionData }) => {
+                        if (!subscriptionData.data) return prev;
+                        const newFeedItem =
+                          subscriptionData.data.on_Add_stage_code;
+                        return Object.assign({}, prev, {
+                          stage_code: [...prev.stage_code, newFeedItem]
+                        });
+                      }
+                    })
+                  }
+                  subscribeToNewComments1={() => {
+                    subscribeToMore({
+                      document: on_Delete_stage_code,
+                      updateQuery: (prev, { subscriptionData }) => {
+                        if (!subscriptionData.data) return prev;
+                        const newFeedItem =
+                          subscriptionData.data.on_Delete_stage_code;
+                        return Object.assign({}, prev, {
+                          stage_code: [
+                            ...prev.stage_code.filter(time => {
+                              if (
+                                time.timestamp <= newFeedItem.timestamp &&
+                                time.code_string !== newFeedItem.code_string
+                              ) {
+                                return time;
                               }
-                              subscribeToNewComments={() =>
-                                subscribeToMore({
-                                  document: on_Add_stage_code,
-                                  updateQuery: (prev, { subscriptionData }) => {
-                                    if (!subscriptionData.data) return prev;
-                                    const newFeedItem =
-                                      subscriptionData.data.on_Add_stage_code;
-                                    return Object.assign({}, prev, {
-                                      stage_code: [
-                                        ...prev.stage_code,
-                                        newFeedItem
-                                      ]
-                                    });
-                                  }
-                                })
-                              }
-                              subscribeToNewComments1={() => {
-                                subscribeToMore({
-                                  document: on_Delete_stage_code,
-                                  updateQuery: (prev, { subscriptionData }) => {
-                                    if (!subscriptionData.data) return prev;
-                                    const newFeedItem =
-                                      subscriptionData.data
-                                        .on_Delete_stage_code;
-                                    let old_data = JSON.parse(
-                                      localStorage.getItem("data")
-                                    );
-                                    if (old_data) {
-                                      old_data = old_data.filter(data => {
-                                        return (
-                                          Number(data.timestamp) <=
-                                          Number(newFeedItem.timestamp)) &&
-                                      (data.stage_code ===
-                                            newFeedItem.stage_code
-                                        );
-                                      });
-                                      localStorage.setItem(
-                                        "data",
-                                        JSON.stringify(old_data)
-                                      );
-                                    }
-                                    return Object.assign({}, prev, {
-                                      stage_code: [
-                                        ...prev.stage_code.filter(time => {
-                                          return (
-                                            Number(time.timestamp) <=
-                                            Number(newFeedItem.timestamp)) &&
-                                            (time.stage_code ===
-                                              newFeedItem.stage_code
-                                          );
-                                        })
-                                      ]
-                                    });
-                                  }
-                                });
-                              }}
-                              loader={loader}
-                            />
-                          </div>
-                        );
-                      }}
-                    </Query>
-                  );
-                }}
-              </Mutation>
+                              return "";
+                            })
+                          ]
+                        });
+                      }
+                    });
+                  }}
+                  loader={loader}
+                />
+              </div>
             );
           }}
-        </Mutation>
+        </Query>
       </div>
     );
   }
